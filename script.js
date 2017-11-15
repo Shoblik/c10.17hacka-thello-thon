41b187248335f0c9aff8e428d71f89dd7718ce48
Francis Redido <sicnarfodider@hotmail.com>
2017-11-15 11:07:42 -0800

    hard coded starting position, adding chipCounter and winCheck functions, chipPlacement function now updates gameArray, added player objects

--- script.js
+++ script.js
@@ -1,31 +1,81 @@
 $(document).ready(initiateOthello);
 
 function initiateOthello(){
-    $('.cells').on('click',chipPlacement)
+    $('.cell').on('click',chipPlacement)
 }
 
 var player=0;
 var blackPlayerStack=32;
 var whitePlayerStack=32;
+var blackPlayer = new Player('black');
+var whitePlayer = new Player('white');
+var gameArr = [
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, 1, null, null, null],
+    [null, null, null, 0, 1, null, null, null],
+    [null, null, null, 1, 0, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null]
+];
+
+function Player(color){
+    this.chipStack=32;
+    this.validTurn=true;
+    this.chipColor=color;
+}
+
+
 
 function chipPlacement(){
     var coordinates = {row: $(this).attr('row'), col: $(this).attr('col')};
-    var blackChip = $('<div>',{
-        'class':'black'
-    });
-    var whiteChip = $('<div>',{
-        'class':'white'
-    });
+    console.log(coordinates);
     if(player===0){
-        $(this).append(blackChip);
+        $(this).append($('<div>',{
+            'class': blackPlayer.chipColor
+        }));
+        gameArr[parseFloat(coordinates.row)][parseFloat(coordinates.col)]=0;
         player+=1;
-        blackPlayerStack-=1;
-        console.log(blackPlayerStack)
+        blackPlayer.chipStack-=1;
+        console.log(blackPlayer.chipStack)
     }else{
-        $(this).append(whiteChip);
+        $(this).append($('<div>',{
+            'class': whitePlayer.chipColor
+        }));
+        gameArr[parseFloat(coordinates.row)][parseFloat(coordinates.col)]=1;
         player-=1;
-        whitePlayerStack-=1;
-        console.log(whitePlayerStack)
+        whitePlayer.chipStack-=1;
+        console.log(whitePlayer.chipStack)
     }
+
     return coordinates;
+}
+
+function chipCounter(arr){ //this'll after flip function
+    var counterObj = {
+        blackCount:0,
+        whiteCount:0
+    };
+    for(i=0;i<arr.length;i++){
+        for(p=0;p<arr[i].length;p++){
+            if(arr[i][p]===0){
+                counterObj.blackCount+=1;
+            }else if(arr[i][p]===1){
+                counterObj.whiteCount+=1
+            }
+        }
+    }
+    return counterObj;
+}
+
+function winCheck(){
+    var currentCounter = chipCounter(gameArr); //returns an object with whiteCount and blackCount
+    if(blackPlayerStack===0 && whitePlayerStack===0 || !blackPlayer.validTurn && !whitePlayer.validTurn){
+        if(currentCounter.blackCount>currentCounter.whiteCount){
+            console.log('black wins')
+        }else{
+            console.log('white wins')
+        }
+    }
 }
